2cba0901c9e19dd225cd5d34666eacb744c560ca
VinayPooja@123 <poojitha6789012@gmail.com>
2023-05-12 10:25:33 +0530

    Added Tests

--- test/Get/GetUsersTest.test.js
+++ test/Get/GetUsersTest.test.js
@@ -4,16 +4,14 @@ const{getAllUsers,getUsersWithLimit,getAllUsersInOurAccount}= require('../../src
 test("Should able to Get All Users", async () => {
   const allUsers = await getAllUsers();
   expect(allUsers.status).toBe(200);
-  console.log(allUsers.data.limit);
-  // expect(allUsers.result.limit).toBe(20);
+  expect(allUsers.data.limit).toBe(20);
 });
 
 test("Should able to Get Users With Specific limit", async () => {
   let limit =10;
   const response = await getUsersWithLimit(limit);
   expect(response.status).toBe(200);
-  expect(response.data.limit).toBe(10);
-  // expect(response.data.limit).toBe(limit);
+  expect(response.data.limit).toBe(limit);
 
 });
 test("Should able to Get All Users In Our Account", async () => {
